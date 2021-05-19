9875cad5cb79beff18de3c0de7f9219fd1c3dfcd
Toluwashe Ajise <digittolu25@gmail.com>
2021-05-19 23:06:30 +0100

    add number solutions

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -38,40 +38,18 @@ end
 print "Hello World"
 puts ""
 p add(2, 3)
-puts ""
-p multiply(786, 92)
+puts multiply(6, 92)
 p add(4, 78)
-
-How to Name Variables
-Ruby is a language that aims to be natural to read and easy to write. Remember this when you’re naming your variables. The name should, as clearly as possible, describe what the value of the variable represents.
-
-Naming variables clearly will pay dividends when you review your code months after you’ve written it, when you can no longer remember what that variable was designed to store. From now on, when naming your variables, remember the following quote by John Woods:
-
-Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live.
-
-The most basic thing you can do to write clean, maintainable code is to name your variables properly. So get into this habit early to avoid psychopath programmers coming after you.
-
-Variable names should always be lowercase, and multiple words that make up a variable name should be split by an underscore. This is known as snake_case.
-
-
-Our programming so far has been very literal.
-
-A 1 is just a 1.
-
-The string "dog" is just the word "dog".
-
-These objects in Ruby are what they are. But take the quotation marks out of "dog". Then, insert an equals sign =
-
-And finally, on that same line, provide a data object on the the right side of that equals sign.
-
 dog = "cat"
-We have just declared a variable with the name of dog. This quote-less (also referred to as a bareword) term, dog, is not a string. But it has been assigned to a string. And that string is "cat":
+puts (dog + " food")
 
-dog = "cat"
-puts dog
-#=> cat
-The variable dog refers to the value "cat". This means that anything that interacts with the variable dog is essentially interacting with the value "cat":
+a = "supercalifragilistic"
+b = "expialidocious"
+puts "The length of the words #{a} and #{b} is:"
+puts a.length + b.length
+puts "#{a} is #{a.length - b.length} characters longer than #{b}"
 
-dog = "cat"
-puts(dog + "food")   
-#=> catfood
+puts "The length of the words #{'supercalifragilistic'} and #{'expialidocious'} is:"
+puts "supercalifragilistic".length + "expialidocious".length
+puts "supercalifragilistic is #{'supercalifragilistic'.length - 'expialidocious'.length} characters longer than expialidocious"
+
