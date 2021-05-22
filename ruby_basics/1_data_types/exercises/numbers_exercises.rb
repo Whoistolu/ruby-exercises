587697b2366f43ca2fb29abd42a0b1036639f384
Toluwashe Ajise <digittolu25@gmail.com>
2021-05-22 20:47:15 +0100

    additional excercise

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -137,28 +137,3 @@ To determine whether an expression evaluates to true or false, you’ll need a c
 
 5.eql?(5.0) #=> false; although they are the same value, one is an integer and the other is a float
 5.eql?(5)   #=> true
-#equal? checks whether both values are the exact same object in memory. This can be slightly confusing because of the way computers store some values for efficiency. Two variables pointing to the same number will usually return true.
-
-a = 5
-b = 5
-a.equal?(b) #=> true
-This expression is true because of the way computers store integers in memory. Although two different variables are holding the number 5, they point to the same object in memory. However, consider the next code example:
-
-a = "hello"
-b = "hello"
-a.equal?(b) #=> false
-This happens because computers can’t store strings in the same efficient way they store numbers. Although the values of the variables are the same, the computer has created two separate string objects in memory.
-
-In addition to the above operators, Ruby has a special operator that is affectionately referred to as the spaceship operator. Unlike the other comparison operators, which all return true or false, the spaceship operator returns one of three numerical values.
-
-<=> (spaceship operator) returns the following:
-
--1 if the value on the left is less than the value on the right;
-0 if the value on the left is equal to the value on the right; and
-1 if the value on the left is greater than the value on the right.
-5 <=> 10    #=> -1
-10 <=> 10   #=> 0
-10 <=> 5    #=> 1
-The spaceship operator is most commonly used in sorting functions, which we’ll cover more later.
-
-All of the above operators also work on data types other than numbers, such as strings. Why not play around with this in a REPL?
